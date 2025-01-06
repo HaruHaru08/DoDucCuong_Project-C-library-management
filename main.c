b27a842d56294f5780922c72a3ceda48fe8e05bb
HaruHaru08 <cuong33442@gmail.com>
2025-01-06 10:06:13 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -5,19 +5,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	struct Book books[50]={
-	{1, "De Men Phieu Luu Ky", "To Hoai",100, 5.00, {1, 6, 1941}},
-	{2, "Truyen Co Tich Viet Nam", "Nhieu Tac Gia",90, 6.00, {1, 1, 1950}},
-	{3, "Tat Den", "Ngo Tat To",85, 4.50, {1, 1, 1939}},
-	{4, "Que Noi", "Vo Quang",70, 5.50, {1, 1, 1974}},
-    {5, "Dat Rung Phuong Nam", "Doan Gioi",65, 6.50, {1, 1, 1957}},
-    {6, "Doraemon", "Fujiko F.Fujio",60, 3.99, {1, 12, 1969}},
-    {7, "Than Dong Dat Viet", "Le Linh",55, 2.50, {1, 1, 2002}},
-    {8, "Conan - Tham Tu Lung Danh", "Gosho Aoyama",50, 4.50, {1, 1, 1994}},
-    {9, "7 Vien Ngoc Rong", "Akira Toriyama",45, 5.00, {1, 11, 1984}},
-    {10, "One Piece", "Eiichiro Oda",40, 5.00, {22, 7, 1997}},
-	};
-	saveBooksToFile(books,10);
+//	struct Book books[50]={
+//	{1, "De Men Phieu Luu Ky", "To Hoai",100, 5.00, {1, 6, 1941}},
+//	{2, "Truyen Co Tich Viet Nam", "Nhieu Tac Gia",90, 6.00, {1, 1, 1950}},
+//	{3, "Tat Den", "Ngo Tat To",85, 4.50, {1, 1, 1939}},
+//	{4, "Que Noi", "Vo Quang",70, 5.50, {1, 1, 1974}},
+//    {5, "Dat Rung Phuong Nam", "Doan Gioi",65, 6.50, {1, 1, 1957}},
+//    {6, "Doraemon", "Fujiko F.Fujio",60, 3.99, {1, 12, 1969}},
+//    {7, "Than Dong Dat Viet", "Le Linh",55, 2.50, {1, 1, 2002}},
+//    {8, "Conan - Tham Tu Lung Danh", "Gosho Aoyama",50, 4.50, {1, 1, 1994}},
+//    {9, "7 Vien Ngoc Rong", "Akira Toriyama",45, 5.00, {1, 11, 1984}},
+//    {10, "One Piece", "Eiichiro Oda",40, 5.00, {22, 7, 1997}},
+//	};
+//	saveBooksToFile(books,10);
+	struct Book books[50];
 	struct member members[100];
 	struct Admin admin={"admin123@gmail.com","admin2508@"};
 	saveAdminToFile(&admin);
@@ -40,8 +41,9 @@ int main(int argc, char *argv[]) {
 					menuBook();
 					printf("Enter The Choice: ");
 					scanf("%d",&choiceMenu);
-					switch(choice){
+					switch(choiceMenu){
 						case 1:
+							loadBooksFromFile(books,&n);
 							printList(books,n);
 							getValidSelection(&select);
 							if(select== '0'){
@@ -54,6 +56,7 @@ int main(int argc, char *argv[]) {
 							break;
 						case 2:
 							addBook(books, &n);
+							saveBooksToFile(books,n);
 							getValidSelection(&select);
 							if(select== '0'){
 								printf("========= Thank You =========\n");
@@ -65,6 +68,7 @@ int main(int argc, char *argv[]) {
 							break;
 						case 3:
 							updateBook(books,n);
+							saveBooksToFile(books,n);
 							getValidSelection(&select);
 							if(select== '0'){
 								
@@ -77,6 +81,7 @@ int main(int argc, char *argv[]) {
 							break;
 						case 4:
 							deleteBook(books,&n);
+							saveBooksToFile(books,n);
 							getValidSelection(&select);
 							if(select== '0'){
 							printf("========= Thank You =========\n");
@@ -124,7 +129,7 @@ int main(int argc, char *argv[]) {
 							printf("Lua chon khong hop le xin hay nhap lai");
 							break;
 					}
-				}while(choice!=0);
+				}while(choiceMenu!=7);
 				break;
 			case 2:
 				do{
